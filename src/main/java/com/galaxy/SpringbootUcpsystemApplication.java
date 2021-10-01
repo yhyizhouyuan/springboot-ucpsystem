170be7c79e0966d3580a214e229329c7f9358fb4
yhyizhouyuan <2017767351@qq.com>
2021-10-01 10:12:24 +0800

    Initial commit

---
+++ src/main/java/com/galaxy/SpringbootUcpsystemApplication.java
@@ -0,0 +1,13 @@
+package com.galaxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUcpsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUcpsystemApplication.class, args);
+    }
+
+}
